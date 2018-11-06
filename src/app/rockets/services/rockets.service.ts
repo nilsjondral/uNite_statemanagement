b4bb41b0cd2df9dcf49d2769f2a747aa80231171
nilsjondral <nils.jondral@telenet.be>
2018-11-06 02:52:44 +0100

    more rockets

--- src/app/rockets/services/rockets.service.ts
+++ src/app/rockets/services/rockets.service.ts
@@ -13,8 +13,12 @@ export class RocketsService {
   private rockets = [
     new Rocket(1, 'Ariane 5', 'ESA', 21000, 10865),
     new Rocket(2, 'Atlas 5', 'ULA', 18500, 8700),
-    new Rocket(3, 'Falcon 9', 'SPACEX', 15960, 5500),
-    new Rocket(4, 'Soyuz ST-B', 'ROSCOSMOS', 9000, 3250),
+    new Rocket(3, 'Delta IV Heavy', 'ULA', 28790, 14220),
+    new Rocket(4, 'Electron', 'ROCKET LAB', 225, 0),
+    new Rocket(5, 'Falcon 9', 'SPACEX', 15960, 5500),
+    new Rocket(6, 'Falcon Heavy', 'SPACEX', 57000, 10000),
+    new Rocket(7, 'Saturn V', 'USA', 140000, 0),
+    new Rocket(8, 'Soyuz ST-B', 'ROSCOSMOS', 9000, 3250),
   ];
 
   constructor() { }
