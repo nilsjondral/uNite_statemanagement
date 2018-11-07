c2598e50008c0350c18187c39813d481ad606a9b
nilsjondral <nils.jondral@telenet.be>
2018-11-07 16:17:30 +0100

    router sync

--- src/app/rockets/components/rockets-list/rockets-list.component.ts
+++ src/app/rockets/components/rockets-list/rockets-list.component.ts
@@ -1,4 +1,4 @@
-import { debounceTime, tap, switchMap } from 'rxjs/operators';
+import { debounceTime, tap, switchMap, share } from 'rxjs/operators';
 import { Rocket } from '../../models/rocket.model';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
@@ -6,6 +6,7 @@ import { Store } from '@ngrx/store';
 import { rocketsQuery } from 'src/app/state/rockets.selectors';
 import { SearchQueryUpdated, RocketsLoaded } from 'src/app/state/rockets.actions';
 import { RocketsService } from '../../services/rockets.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-rockets-list',
@@ -14,16 +15,32 @@ import { RocketsService } from '../../services/rockets.service';
 })
 export class RocketsListComponent implements OnInit {
   rockets$: Observable<Rocket[]>;
-  searchQuery$: Observable<string>;
   loading$: Observable<boolean>;
+  searchQuery$: Observable<string>;
 
   constructor(
     private store: Store<any>,
-    private rocketsService: RocketsService) { }
+    private rocketsService: RocketsService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     this.store.dispatch(new RocketsLoaded([]));
-    this.store.select(rocketsQuery.getQuery).pipe(
+
+    const currentQuerry = this.route.snapshot.queryParams['q'];
+    this.store.dispatch(new SearchQueryUpdated(currentQuerry));
+    this.searchQuery$ = this.store.select(rocketsQuery.getQuery);
+    this.searchQuery$.subscribe(q => {
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: {
+          ...this.route.snapshot.queryParams,
+          q: q
+        }
+      });
+    });
+
+    this.searchQuery$.pipe(
       debounceTime(500),
       switchMap(q => this.rocketsService.getRockets(q)))
       .subscribe(rockets => this.store.dispatch(new RocketsLoaded(rockets)));
